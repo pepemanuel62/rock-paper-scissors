7663de8a4f6b4aea88b02b5472d173a213e41d01
pepemanuel62 <199387063+pepemanuel62@users.noreply.github.com.>
2025-04-17 09:20:38 -0400

    Create js file

---
+++ main.js
@@ -0,0 +1,18 @@
+//Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
+
+/*Create a new function named getComputerChoice.
+Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1. Think about how you can 
+use this to conditionally return one of the multiple choices. 
+Note: Some learners might think that arrays are necessary for this problem,
+but they aren’t. If you’re unfamiliar with arrays, don’t worry - they’ll be covered later in the curriculum.
+Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.*/
+
+ //function getComputerChoice() {}
+
+ 
+
+ 
+
+
+ 
